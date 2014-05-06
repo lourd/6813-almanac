5472c491d9f7656d0fce3bada4e6ee5e9d15344d
Guillaume Kugener <kugenerguillaume@gmail.com>
2014-05-06 01:52:56 -0400

    fixed the carousel layout and interactions

--- client/views/racks/quick_stats.js
+++ client/views/racks/quick_stats.js
@@ -1,20 +1,25 @@
 Template.quickStats.rendered = function() {
-	$("#stats-container").selectable();
+	$("#stats-container").selectable({
+		selected: function(event, ui) {
+			var text = $(ui.selected).children()[0].innerHTML;
+
+			Session.set('graphPlot', Session.get('currentPlot'));
+			Session.set('graphType', text);
+		},
+
+		unselected: function(event, ui) {
+
+		}
+	});
 }
 
 
 Template.quickStats.helpers({
-	// stats: function() {
-	// 	var current = Session.get('currentPlot');
+	stats: function() {
+		var current = Session.get('currentPlot');
+		var plot = Plots.findOne({name: current});
 
-	// 	if (current === undefined) {
-	// 		Session.set('currentPlot', 'Plot 3');
-	// 	}
-	// 	console.log(current);
-
-	// 	var plot = Plots.findOne({name: current});
-
-	// 	return plot.stats;
-	// }
+		return plot.stats;
+	}
 });
 
