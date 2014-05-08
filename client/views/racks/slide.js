22131b108fe9acc73e6571e408a5f167054b4267
Louis DeScioli <louis.descioli@gmail.com>
2014-05-08 03:39:24 -0400

    Live data for rack page

--- client/views/racks/slide.js
+++ client/views/racks/slide.js
@@ -1,7 +1,10 @@
+var MAX_HEIGHT = 250;
+var MIN_WIDTH = 100;
+var MAX_WIDTH = 600;
+var MIN_HEIGHT = 80;
+
 Template.slide.helpers({
-	isGraph: function(plotName) {
-		return false;
-	}
+	
 });
 
 Template.slide.rendered = function() {
@@ -32,11 +35,30 @@ Template.slide.destroyed = function () {
 //////////////////////////////////////////////////
 // Plots
 //////////////////////////////////////////////////
+Template.plotSlide.helpers({
+	scaledWidth: function () {
+		// Get the height & width of the rack
+		var rack = Racks.findOne(); // works cause we're only subscribed to the one current rack
+		var aspect = rack.attributes.height / rack.attributes.width;
+		// scale the div correctly
+		if (aspect > 0.5) {	// if it's a tall rectangle
+			return Math.max(MAX_HEIGHT/aspect, MIN_WIDTH);
+		} else { // if it's a fat rectangle
+			return MAX_WIDTH;
+		}
+	},
+	scaledHeight: function() {
+		var rack = Racks.findOne();
+		var aspect = rack.attributes.height / rack.attributes.width;
+		if (aspect > 0.5) {
+			return MAX_HEIGHT;
+		} else {
+			return Math.max(aspect*MAX_WIDTH, MIN_HEIGHT);
+		}
+	}
+});
 Template.plotSlide.rendered = function() {
-	// var el = this.firstNode;
-	// $('#plot-carousel').slickAdd(el);
-	console.log("plot slide rendered");
-
+	
 }
 
 Template.plotSlide.created = function() {
