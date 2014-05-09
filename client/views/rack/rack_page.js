440dd2261d74cb0d65cd51b75135ae9e29e662d7
Matt Hunter <matthunter1217@gmail.com>
2014-05-09 02:21:32 -0400

    graph now works from sensor data

--- client/views/rack/rack_page.js
+++ client/views/rack/rack_page.js
@@ -12,8 +12,7 @@ Template.rackPage.helpers({
 
 Template.rackPage.rendered = function() {
 
-	createGraph = function() {
-
+	createRealGraph = function(){
 		// this is where the graph will be drawn
 		var plotDisplay = $(".plot-display");
 
@@ -21,28 +20,37 @@ Template.rackPage.rendered = function() {
 		var h = 250;
 		//var w = plotDisplay.width();
 		//var h = plotDisplay.height();
-		var padding = 30;
+		var padding = 35;
 
-		// dummy data set 
-		// TODO: replace with reading collection
-		var dataset = [
-            [5, 20], [480, 90], [250, 50], [100, 33], [330, 95],
-            [410, 12], [475, 44], [25, 67], [85, 21], [220, 88]
-        ];
+            // open all readings by Temperature
+            // TODO: change this to work with any parameter
+            var readings = Readings.find({type: "temperature"}, {sort: {recorded_at: 1}});
 
-        // these functions scale our data
-        var xScale = d3.scale.linear()
-       		.domain([0, d3.max(dataset, function(d) { return d[0]; })])
-       		.range([padding, w - padding]);
+            console.log("Number of Readings: " + readings.count());
+
+            var dataset = [];
+            readings.forEach(function(reading){
+                  dataset.push([reading.recorded_at, reading.value]);
+            });
+
+            var minDate = new Date(dataset[0][0]);
+            var maxDate = new Date(dataset[dataset.length - 1][0]);
+
+            var xScale = d3.time.scale()
+                  .domain([minDate, maxDate])
+                  .range([padding, w - padding]);
+
+            var minValue = d3.min(dataset, function(d) { return d[1];});
+            var maxValue = d3.max(dataset, function(d) { return d[1];});
+            var yAxisBuffer = .25 * (maxValue - minValue);
 
        	var yScale = d3.scale.linear()
-       		.domain([0, d3.max(dataset, function(d) { return d[1]; })])
+       		.domain([minValue - yAxisBuffer, maxValue + yAxisBuffer])
        		.range([h - padding, padding]);
 
        	var xAxis = d3.svg.axis()
        		.scale(xScale)
-       		.orient("bottom")
-       		.ticks(5);
+       		.orient("bottom");
 
        	var yAxis = d3.svg.axis()
        		.scale(yScale)
@@ -61,7 +69,7 @@ Template.rackPage.rendered = function() {
        		.enter()
        		.append("circle")
        		.attr("cx", function(d) {
-       			return xScale(d[0]);
+       			return xScale(new Date(d[0]));
        		})
        		.attr("cy", function(d) {
        			return yScale(d[1]);
@@ -77,88 +85,10 @@ Template.rackPage.rendered = function() {
        		.attr("class", "axis")
        		.attr("transform", "translate(" + padding + ",0)")
        		.call(yAxis);
-	};
-
-	createRealGraph = function(){
-		// this is where the graph will be drawn
-		var plotDisplay = $("#d3-anchor");
-
-		var w = 600;
-		var h = 250;
-		//var w = plotDisplay.width();
-		//var h = plotDisplay.height();
-		var padding = 30;
-
-		// dummy data set 
-		// TODO: replace with reading collection
-		var dataset = [
-            [5, 20], [480, 90], [250, 50], [100, 33], [330, 95],
-            [410, 12], [475, 44], [25, 67], [85, 21], [220, 88]
-        ];
-
-        // these functions scale our data
-        var xScale = d3.scale.linear()
-       		.domain([0, d3.max(dataset, function(d) { return d[0]; })])
-       		.range([padding, w - padding]);
-
-       	var yScale = d3.scale.linear()
-       		.domain([0, d3.max(dataset, function(d) { return d[1]; })])
-       		.range([h - padding, padding]);
-
-       	var xAxis = d3.svg.axis()
-       		.scale(xScale)
-       		.orient("bottom")
-       		.ticks(5);
-
-       	var yAxis = d3.svg.axis()
-       		.scale(yScale)
-       		.orient("left")
-       		.ticks(5);
-
-       	var readings = Readings.find();
-
-       	var count = 5;
-       	for (var i = 0; i < readings.length(); i++) {
-       		console.log(count);
-       		console.log(readings[i].value);
-       		count++;
-       	}
-       	/*
-
-       	// add an svg (canvasesque) element to our div anchor
-       	var svg = d3.select(".plot-display")
-       		.append("svg")
-       		.attr("width", w)
-       		.attr("height", h);
-
-       	// draws a circle at every point
-       	svg.selectAll("circle")
-       		.data(dataset)
-       		.enter()
-       		.append("circle")
-       		.attr("cx", function(d) {
-       			return xScale(d[0]);
-       		})
-       		.attr("cy", function(d) {
-       			return yScale(d[1]);
-       		})
-       		.attr("r", 5);
-
-       	svg.append("g")
-       		.attr("class", "axis")
-       		.attr("transform", "translate(0," + (h - padding) + ")")
-       		.call(xAxis);
-
-       	svg.append("g")
-       		.attr("class", "axis")
-       		.attr("transform", "translate(" + padding + ",0)")
-       		.call(yAxis);
-       	*/
 	};
 
 	// draws the graph
 	// TODO: trigger this call when a particular trait is selected
-	createGraph();
 	createRealGraph();
 
 	// Save the data context, a Rack
