817ff05be2d42c054add56b799496e39a0e05f74
Matt Hunter <matthunter1217@gmail.com>
2014-05-09 00:34:38 -0400

    fixed conflicts

--- client/views/rack/rack_page.js
+++ client/views/rack/rack_page.js
@@ -11,16 +11,16 @@ Template.rackPage.helpers({
 });
 
 Template.rackPage.rendered = function() {
-	
+
 	createGraph = function() {
-		//var w = 600;
-		//var h = 250;
 
 		// this is where the graph will be drawn
 		var plotDisplay = $(".plot-display");
 
-		var w = plotDisplay.width();
-		var h = plotDisplay.height();
+		var w = 600;
+		var h = 250;
+		//var w = plotDisplay.width();
+		//var h = plotDisplay.height();
 		var padding = 30;
 
 		// dummy data set 
@@ -79,9 +79,87 @@ Template.rackPage.rendered = function() {
        		.call(yAxis);
 	};
 
+	createRealGraph = function(){
+		// this is where the graph will be drawn
+		var plotDisplay = $("#d3-anchor");
+
+		var w = 600;
+		var h = 250;
+		//var w = plotDisplay.width();
+		//var h = plotDisplay.height();
+		var padding = 30;
+
+		// dummy data set 
+		// TODO: replace with reading collection
+		var dataset = [
+            [5, 20], [480, 90], [250, 50], [100, 33], [330, 95],
+            [410, 12], [475, 44], [25, 67], [85, 21], [220, 88]
+        ];
+
+        // these functions scale our data
+        var xScale = d3.scale.linear()
+       		.domain([0, d3.max(dataset, function(d) { return d[0]; })])
+       		.range([padding, w - padding]);
+
+       	var yScale = d3.scale.linear()
+       		.domain([0, d3.max(dataset, function(d) { return d[1]; })])
+       		.range([h - padding, padding]);
+
+       	var xAxis = d3.svg.axis()
+       		.scale(xScale)
+       		.orient("bottom")
+       		.ticks(5);
+
+       	var yAxis = d3.svg.axis()
+       		.scale(yScale)
+       		.orient("left")
+       		.ticks(5);
+
+       	var readings = Readings.find();
+
+       	var count = 5;
+       	for (var i = 0; i < readings.length(); i++) {
+       		console.log(count);
+       		console.log(readings[i].value);
+       		count++;
+       	}
+       	/*
+
+       	// add an svg (canvasesque) element to our div anchor
+       	var svg = d3.select(".plot-display")
+       		.append("svg")
+       		.attr("width", w)
+       		.attr("height", h);
+
+       	// draws a circle at every point
+       	svg.selectAll("circle")
+       		.data(dataset)
+       		.enter()
+       		.append("circle")
+       		.attr("cx", function(d) {
+       			return xScale(d[0]);
+       		})
+       		.attr("cy", function(d) {
+       			return yScale(d[1]);
+       		})
+       		.attr("r", 5);
+
+       	svg.append("g")
+       		.attr("class", "axis")
+       		.attr("transform", "translate(0," + (h - padding) + ")")
+       		.call(xAxis);
+
+       	svg.append("g")
+       		.attr("class", "axis")
+       		.attr("transform", "translate(" + padding + ",0)")
+       		.call(yAxis);
+       	*/
+	};
+
 	// draws the graph
 	// TODO: trigger this call when a particular trait is selected
 	createGraph();
+	createRealGraph();
 
 	// Save the data context, a Rack
 	var rackData = this.data;
