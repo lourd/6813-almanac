f19f462a8942b5650ee6fef846d9b3bde35b456c
Louis DeScioli <louis.descioli@gmail.com>
2014-05-13 12:53:30 -0400

    Rack page returns to area page on running out of plots

--- client/views/rack/rack_page.js
+++ client/views/rack/rack_page.js
@@ -20,32 +20,6 @@ Template.rackPage.rendered = function() {
 	// Save the data context, a Rack
 	var rackData = this.data;
 
-	$("#add-plot").click(function () {
-		// Open the add plot modal
-		$("#add-plot-popup").dialog("open");
-	});
-
-	/*
-	 *	Listener for the remove plot button
-	*/
-	$("#remove-plot").click(function () {
-		Session.set('graphPlot', -1);
-		var plotId = Template.rackPage.getCurrentPlotId();
-		var carousel = $("#plot-carousel");
-		// Get the current slide/plot index in the Stack
-		var stackIndex = carousel.slickCurrentSlide();
-
-		// Unslick the slide
-		// So many bugs...
-		console.log("unslick called");
-		carousel.unslick();
-
-		Meteor.call('deletePlot', plotId, rackData._id, stackIndex, function(error, id) {
-			// Error handling!
-		});
-		Template.carousel.slickify();
-	});
-
 	/*
 	 *	Options and actions for the add plot modal
 	*/
@@ -60,6 +34,7 @@ Template.rackPage.rendered = function() {
 					rackId: rackData._id,
 					name: plotName.value
 				};
+				// Have to unslick and reslick to avoid bugs in slick
 				$("#plot-carousel").unslick()
 				// Add it with a Meteor.call
 				Meteor.call('addPlot', newPlot, function(error, result) {
@@ -68,10 +43,12 @@ Template.rackPage.rendered = function() {
 					} 
 				});
 				Template.carousel.slickify();
+				$("#plot-carousel").slickGoTo(Plots.find().count()-1);
 
 				// Reset the modal text box and close
 				plotName.value = "";
 				$(this).dialog("close");
+				// Go to the newly added slide
 
 			}, "Cancel": function () {
 				$(this).dialog("close");
@@ -80,5 +57,35 @@ Template.rackPage.rendered = function() {
 	});
 
 }
+
+Template.rackPage.events({
+	'click #add-plot': function () {
+		$("#add-plot-popup").dialog("open");
+	},
+
+	'click #remove-plot': function() {
+		Session.set('graphPlot', -1);
+		var rackData = this;
+		var plotId = Template.rackPage.getCurrentPlotId();
+		var carousel = $("#plot-carousel");
+		// Get the current slide/plot index in the Stack
+		var stackIndex = carousel.slickCurrentSlide();
+
+		// Unslick the slide then reslick
+		carousel.unslick();
+
+		Meteor.call('deletePlot', plotId, rackData._id, stackIndex, function(error, id) {
+			// If the rack has no more plots, go back to the area page and delete the rack
+			if (Plots.find().count() === 0) {
+				Router.go('areaPage', {_id: rackData.areaId});
+				Racks.remove({_id: rackData._id}, function() {})
+			}
+		});
+		Template.carousel.slickify();
+		// Either the next one or the last one
+		var nextPlotNum = Math.min(Plots.find().count()-1, stackIndex);
+		carousel.slickGoTo(nextPlotNum);
+	}
+});
 
 
