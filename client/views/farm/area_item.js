578e8b074acfa2717be74b3f2865a769459f1a7e
Louis DeScioli <louis.descioli@gmail.com>
2014-05-13 21:39:27 -0400

    Can now add farms

--- client/views/farm/area_item.js
+++ client/views/farm/area_item.js
@@ -1,6 +1,19 @@
-
 Template.areaItem.helpers({
     stats: function() {
         return this.stats;
+    }
+});
+
+Template.addArea.events({
+    'click button': function (evt) {
+        evt.preventDefault();
+        var area = {
+            name: $('#farm-name-input').val(),
+            location: $('#farm-location-input').val()
+        }
+        debugger
+        Meteor.call('newArea', area, function(error, id) {
+            // error handling!
+        })
     }
 });
--- collections/areas.js
+++ collections/areas.js
@@ -1,1 +1,7 @@
 Areas = new Meteor.Collection('areas');
+
+Meteor.methods({
+    newArea: function (doc) {
+        return Areas.insert(doc);
+    }
+});
